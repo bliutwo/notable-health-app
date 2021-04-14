8133dd579569cd2fb0ffbf7d2fb5300d3e1f5d75
bliutwo <bliutwo@cs.stanford.edu>
2021-04-13 20:56:39 -0700

    Finish what I have before submitting

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,34 @@
 from django.db import models
 
+import csv
+
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class DataParser(models.Model):
+    def __init__(self):
+        self.patient_data = self.get_data("patients.csv")
+        self.physician_data = self.get_data("physicians.csv")
+        self.physicians = self.get_physicians()
+
+    def get_data(self, csv_filename):
+        data = []
+        with open(csv_filename) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                data.append(row)
+        return data
+
+    def get_physicians(self):
+        physicians = []
+        line_count = 0
+        print(self.physician_data)
+        for row in self.physician_data:
+            if line_count != 0:
+                print(row[0])
+                physicians.append(row[0])
+            line_count += 1
+        print(physicians)
+        return physicians
--- hello/views.py
+++ hello/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .models import DataParser
 
 # Create your views here.
 def index(request):
@@ -16,6 +17,15 @@ def index(request):
     # times = int(os.environ.get('TIMES',3))
     # return HttpResponse('Hello! ' * times)
     return render(request, "notable.html")
+
+def results(request):
+    dp = DataParser()
+    context = {
+        'patient_data': dp.patient_data,
+        'physician_data': dp.physician_data,
+        'physicians': dp.physicians,
+    }
+    return render(request, "results.html", context)
 
 
 def db(request):
